fefff8754f31f31b63bcc7d436a7839377e269b5
Luiz Eduardo Kauan Da Silva <Luiz.trindade2002@outlook.com>
2021-09-08 21:51:40 -0300

    Adicionar arquivos de projeto.

---
+++ projeto_03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace projeto_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string funcionario;
+            double salario, aumento;
+
+            Console.WriteLine("Digite o nome do funcionário: ");
+            funcionario = Console.ReadLine();
+
+            Console.WriteLine("Digite o salário do funcionário: ");
+            salario = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a porcentagem de aumento do salário do funcionário: ");
+            aumento = Convert.ToDouble(Console.ReadLine());
+
+            salario = salario + (salario * (aumento / 100));
+
+            Console.WriteLine("O novo salário do funcionário " + funcionario + " é de: R$" + salario);
+        }
+    }
+}
